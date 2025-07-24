d67a73964c8a1f328ee796178d52a9634f07312e
jedtapungkee <jedtapungkee.c@ku.th>
2025-07-25 00:31:31 +0700

    Add user cart api

--- controllers/user.js
+++ controllers/user.js
@@ -1,77 +1,160 @@
-const prisma = require('../config/prisma')
+const prisma = require("../config/prisma");
 
-exports.listUsers = async(req,res)=>{
-    try {
-        const users = await prisma.user.findMany(
-            {
-                orderBy: {
-                    createdAt: 'desc'
-                },
-                select:{
-                    id: true,
-                    firstName: true,
-                    lastName: true,
-                    email: true,
-                    role: true,
-                    enabled: true,
-                    picture: true,
-                    createdAt: true,
-                }
-            }
-        );
-        res.json({ users });
-    } catch (error) {
-        console.error("Error listing users:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
+exports.listUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        role: true,
+        enabled: true,
+        picture: true,
+        createdAt: true,
+      },
+    });
+    res.json({ users });
+  } catch (error) {
+    console.error("Error listing users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-exports.changeStatus = async(req,res)=>{
-    try {
-        const { id, enabled } = req.body;
+exports.changeStatus = async (req, res) => {
+  try {
+    const { id, enabled } = req.body;
 
-        // Find user by ID and update status
-        const user = await prisma.user.update({
-            where: { id },
-            data: { enabled },
-            select:{
-                enabled: true,
-                id: true,
-            }
-        });
+    // Find user by ID and update status
+    const user = await prisma.user.update({
+      where: { id },
+      data: { enabled },
+      select: {
+        enabled: true,
+        id: true,
+      },
+    });
 
-        // console.log(`User status updated: ${user.id} - Enabled: ${user.enabled}`);
-        res.json({
-            message: "User status updated successfully",
-            user,
-        });
-    } catch (error) {
-        console.error("Error changing user status:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-}
+    // console.log(`User status updated: ${user.id} - Enabled: ${user.enabled}`);
+    res.json({
+      message: "User status updated successfully",
+      user,
+    });
+  } catch (error) {
+    console.error("Error changing user status:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-exports.changeRole = async(req,res)=>{
-    try {
-        const { id, role } = req.body;
+exports.changeRole = async (req, res) => {
+  try {
+    const { id, role } = req.body;
 
-        // Find user by ID and update role
-        const user = await prisma.user.update({
-            where: { id },
-            data: { role },
+    // Find user by ID and update role
+    const user = await prisma.user.update({
+      where: { id },
+      data: { role },
+      select: {
+        id: true,
+        role: true,
+      },
+    });
+
+    // console.log(`User role updated: ${user.id} - Role: ${user.role}`);
+    res.json({
+      message: "User role updated successfully",
+      user,
+    });
+  } catch (error) {
+    console.error("Error changing user role:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.userCarts = async (req, res) => {
+  try {
+    const { cart } = req.body;
+
+    const user = await prisma.user.findFirst({
+      where: {
+        id: Number(req.user.id),
+      },
+    });
+
+    //check quantity
+    for (const item of cart) {
+      const product = await prisma.product.findUnique({
+        where: {
+          id: Number(item.id),
+        },
+        select: {
+          title: true,
+          productsizes: {
             select: {
-                id: true,
-                role: true,
-            }
+              size: true,
+              quantity: true,
+              price: true,
+            },
+          },
+        },
+      });
+      
+      if (!product || item.count > product.quantity) {
+        return res.status(400).json({
+          ok: false,
+          message: `ขออภัย สินค้า${product.title || "product"}หมดสต๊อกแล้ว`,
         });
-
-        // console.log(`User role updated: ${user.id} - Role: ${user.role}`);
-        res.json({
-            message: "User role updated successfully",
-            user,
-        });
-    } catch (error) {
-        console.error("Error changing user role:", error);
-        res.status(500).json({ message: "Internal server error" });
+      }
     }
-}
+
+    // delete old carts item
+    await prisma.productOnCart.deleteMany({
+      where: {
+        cart: {
+          orderedById: user.id,
+        },
+      },
+    });
+
+    // delete old carts
+    await prisma.cart.deleteMany({
+      where: {
+        orderedById: user.id,
+      },
+    });
+
+    // เตรียมสินค้า
+    let products = cart.map((item) => ({
+      productId: item.id,
+      count: item.count,
+      price: item.price,
+      size: item.size
+    }));
+
+
+    // หาผลรวม
+    let cartTotal = products.reduce(
+      (sum, item) => sum + item.price * item.count,
+      0
+    );
+
+    // สร้าง cart ใหม่
+    const newcart = await prisma.cart.create({
+      data: {
+        products: {
+          create: products,
+        },
+        cartTotal,
+        orderedById: user.id,
+      },
+    });
+
+    res.send({ message: "Add to cart Successfully" });
+  } catch (error) {
+    console.error("Error fetching user carts:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { listUsers, changeStatus, changeRole } = require('../controllers/user');
+const { listUsers, changeStatus, changeRole, userCarts } = require('../controllers/user');
 const router = express.Router();
 const { adminCheck, authCheck } = require('../middlewares/authCheck');
 
 router.get('/users',listUsers)
 router.post('/change-status',authCheck,adminCheck,changeStatus)
 router.post('/change-role',authCheck,adminCheck,changeRole)
+router.post('/user/cart', authCheck, userCarts)
+
+
+
+
 module.exports = router;
